b1469d951218175e13ff342294d6e9929add95ff
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-01-27 16:34:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,9 +5,9 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
 
+//create 5 var bob and rope objects
 
-var bobObject1,bobObject2,bobObject3, bobObject4,bobObject5, roofObject
-var rope1,rope2,rope3, rope4,rope5;
+
 var world;
 
 
@@ -25,11 +25,10 @@ function setup() {
 
 	startBobPositionX=width/2;
 	startBobPositionY=height/4+500;
-	bobObject1=new bob(startBobPositionX-bobDiameter*2,startBobPositionY,bobDiameter);
-	bobObject2=new bob(startBobPositionX-bobDiameter,startBobPositionY,bobDiameter);
-	bobObject3=new bob(startBobPositionX,startBobPositionY,bobDiameter);
-	bobObject4=new bob(startBobPositionX+bobDiameter,startBobPositionY,bobDiameter);
-	bobObject5=new bob(startBobPositionX+bobDiameter*2,startBobPositionY,bobDiameter);
+	
+	//create bob ojects calculating position with diameters
+	
+	
 	
 
 	var render = Render.create({
@@ -43,11 +42,17 @@ function setup() {
 	});
 
 
-	rope1=new rope(bobObject1.body,roofObject.body,-bobDiameter*2, 0)
-	rope2=new rope(bobObject2.body,roofObject.body,-bobDiameter*1, 0)
-	rope3=new rope(bobObject3.body,roofObject.body,0, 0)
-	rope4=new rope(bobObject4.body,roofObject.body,bobDiameter*1, 0)
-	rope5=new rope(bobObject5.body,roofObject.body,bobDiameter*2, 0)
+	//create ropes attached to the roof
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 	/*constraint1={
 		bodyA:bobObject1.body,
@@ -103,22 +108,21 @@ function draw() {
   background(230);
   roofObject.display();
 
-  rope1.display()
-  rope2.display()
-  rope3.display()
-  rope4.display()
-  rope5.display()	
-  bobObject1.display();
-  bobObject2.display();
-  bobObject3.display();
-  bobObject4.display();
-  bobObject5.display(); 
+  //display rope and bobs
+	
+	
+	
+	
+	
+	
+	
+	
 }
 
 function keyPressed() {
   	if (keyCode === UP_ARROW) {
 
-    	Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-45});
+    	//apply force to right bob
 
   	}
 }
